d10ae98a1012759b1940efea1b5037e828df92b0
AXun <2722636556@qq.com>
2019-12-09 11:11:04 +0800

    添加iods子工程

---
+++ database-migration/01-Code/database-migration/iods/src/main/java/com/xunsoft/iods/IodsApplication.java
@@ -0,0 +1,13 @@
+package com.xunsoft.iods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IodsApplication.class, args);
+    }
+
+}
---
+++ database-migration/01-Code/database-migration/iods/src/test/java/com/xunsoft/iods/IodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xunsoft.iods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
