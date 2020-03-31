18387db03ff1ca1ee398a264c64bf7773bd49a21
GeeKeeper <geekkeeper@163.com>
2020-03-31 14:43:30 +0800

    1.添加系统配置表(BD_SYSTEM_CONFIG)。 2.服务状态表(PM_SERVICE_STATE):添加“进程名称(PROCESS_NAME)”字段。 3.模块列表删除冗余字段。 4.修改节点状态、进程状态和服务状态的状态数据类型。

--- 01-Code/migration/huayou/src/main/java/com/hollysys/common/Application.java
+++ 01-Code/migration/huayou/src/main/java/com/hollysys/common/Application.java
@@ -5,5 +5,10 @@ public class Application {
     public static void main(String[] args) {
 
         jacobean.textToSpeech("java文字转语音播报功能的实现方法");
+        int i =10;
+        int aa = TagDataType.valueOf("BOOL").ordinal();
+        String dataTypeName = TagDataType.values()[i].name();
+        System.out.println(aa);
+        System.out.println(dataTypeName);
     }
 }
